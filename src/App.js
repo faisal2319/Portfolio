9ed71861ff366268cf993d071e0d9e84e8e1d9de
Faisal AlYahya <faisalalyahya23@hotmail.com>
2023-02-01 17:25:43 +0300

    added projects section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import './style.css'
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
+import Projects from "./Components/Projects";
 export default function App(){
     return(
         <div className="main">
             <Navbar/>
-            <About/>
+            <div className="content">
+                <About/>
+                <div className="project-section">
+                    <Projects/>
+                    <Projects/>
+                    <Projects/>
+                </div>
+            </div>
+            
+            
         </div>
         )
 }
---
+++ src/Components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Projects(){
+    return(
+        <div className="projects">
+            <a href="#">
+                <div className="project-info">
+                    <h1>Project Title</h1>
+                </div>
+            </a>
+        </div>
+    )
+}
